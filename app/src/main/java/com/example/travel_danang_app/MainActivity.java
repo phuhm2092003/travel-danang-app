67203ca50b32bac1900b349ff4711adc7c498313
DELL <hominphuzxc@gmail.com>
2023-08-04 13:27:25 +0700

    Upload file

--- app/src/main/java/com/example/travel_danang_app/MainActivity.java
+++ app/src/main/java/com/example/travel_danang_app/MainActivity.java
@@ -1,26 +1,58 @@
 package com.example.travel_danang_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 
 import com.example.travel_danang_app.databinding.ActivityMainBinding;
-import com.example.travel_danang_app.ui.singin.SignInActivity;
-import com.google.firebase.auth.FirebaseAuth;
+import com.example.travel_danang_app.ui.user.FavouriteFragment;
+import com.example.travel_danang_app.ui.user.HomeFragment;
+import com.example.travel_danang_app.ui.user.SettingFragment;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
+        switchFragment(new HomeFragment());
 
-        mainBinding.textView.setOnClickListener(view -> {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(this, SignInActivity.class));
-            finish();
+        mainBinding.bottomMenuNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menu_home:
+                    switchFragment(new HomeFragment());
+                    setBottonNavIcons(R.drawable.ic_home_fill, R.drawable.ic_favourite_line, R.drawable.ic_setting_line);
+                    return true;
+                case R.id.menu_favourite:
+                    switchFragment(new FavouriteFragment());
+                    setBottonNavIcons(R.drawable.ic_home_line, R.drawable.ic_favourite_fill, R.drawable.ic_setting_line);
+                    return true;
+                case R.id.menu_setting:
+                    switchFragment(new SettingFragment());
+                    setBottonNavIcons(R.drawable.ic_home_line, R.drawable.ic_favourite_line, R.drawable.ic_setting_fill);
+                    return true;
+                default:
+                    return false;
+            }
         });
+    }
+
+    private void setBottonNavIcons(int imgResourceHome, int imgResourceFavourite, int imgResourceSetting) {
+        mainBinding.bottomMenuNavigation.getMenu().findItem(R.id.menu_home).setIcon(imgResourceHome);
+        mainBinding.bottomMenuNavigation.getMenu().findItem(R.id.menu_favourite).setIcon(imgResourceFavourite);
+        mainBinding.bottomMenuNavigation.getMenu().findItem(R.id.menu_setting).setIcon(imgResourceSetting);
+    }
+
+    public void switchFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_container, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/travel_danang_app/ui/user/SettingFragment.java
@@ -0,0 +1,84 @@
+package com.example.travel_danang_app.ui.user;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.example.travel_danang_app.R;
+import com.example.travel_danang_app.databinding.FragmentSettingBinding;
+import com.example.travel_danang_app.ui.signup.SignUpActivity;
+import com.example.travel_danang_app.ui.singin.SignInActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SettingFragment extends Fragment {
+    private FragmentSettingBinding settingBinding;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentUser;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        settingBinding = FragmentSettingBinding.inflate(inflater, container, false);
+        View view = settingBinding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        fetchCurrentUserInfo();
+        setListeners();
+    }
+
+    private void fetchCurrentUserInfo() {
+        currentUser= firebaseAuth.getCurrentUser();
+        if(currentUser == null){
+            return;
+        }
+        Glide.with(getContext())
+                .load(currentUser.getPhotoUrl())
+                .centerCrop()
+                .placeholder(R.drawable.photo_default)
+                .into(settingBinding.photoCurrentUser);
+        settingBinding.nameCurrentUser.setText(currentUser.getDisplayName());
+        settingBinding.emailCurrentUser.setText(currentUser.getEmail());
+    }
+
+    private void setListeners(){
+        settingBinding.editProfileButton.setOnClickListener(view -> onEditProfileButtonClicked());
+        settingBinding.changePasswordButton.setOnClickListener(view -> onChangePasswordButtonClicked());
+        settingBinding.logoutButton.setOnClickListener(view -> onLogoutButtonClicked());
+    }
+
+    private void onEditProfileButtonClicked() {
+    }
+
+    private void onChangePasswordButtonClicked() {
+    }
+
+    private void onLogoutButtonClicked() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setMessage("Bạn có muốn đăng xuất?");
+        builder.setPositiveButton("Có", (dialogInterface, i) -> {
+            firebaseAuth.signOut();
+            startActivity(new Intent(getContext(), SignInActivity.class));
+            getActivity().finish();
+        });
+        builder.setNegativeButton("Huỷ", (dialogInterface, i) -> {});
+
+        Dialog dialog = builder.create();
+        dialog.show();
+    }
+}
